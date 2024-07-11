986910262b05110f43d4e065a4ab2a2ac7ed91e2
helozen <shaktishihab247#gmail.com>
2024-07-12 05:53:10 +0600

    Returning json, getting json data and redirect

--- Practice1/views.py
+++ Practice1/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify, redirect, url_for
 
 views = Blueprint(__name__, "views")
 
@@ -9,3 +9,16 @@ def home():
 @views.route("/profile/<username>")
 def profile(username):
     return render_template("index.html", name=username)
+
+@views.route("/json")
+def get_json():
+    return jsonify({'name': 'Shakti', 'coolness': 100})
+
+@views.route("/data")
+def get_data():
+    data = request.json
+    return jsonify(data)
+
+@views.route("/go-to-home")
+def go_to_home():
+    return redirect(url_for(views.home))
