dea14cf259ee704b85a69a74f62941a704b485f5
helozen <shaktishihab247#gmail.com>
2024-07-12 03:53:20 +0600

    Trying Views

--- Practice1/app.py
+++ Practice1/app.py
@@ -1,11 +1,8 @@
 from flask import Flask
+from views import views
 
 app = Flask(__name__)
-
-
-@app.route("/")
-def home():
-    return "This is just for practice"
+app.register_blueprint(views, url_prefix="/views")
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
--- Practice1/views.py
+++ Practice1/views.py
@@ -1,4 +1,7 @@
 from flask import Blueprint
 
-views = Blueprint("views")
+views = Blueprint(__name__, "views")
 
+@views.route("/")
+def home():
+    return "Home Page"
