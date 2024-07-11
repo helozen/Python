a3c6b162559836970b8bb9819b79c3ff662d4cfc
helozen <shaktishihab247#gmail.com>
2024-07-12 05:12:37 +0600

    Adding Parameters

--- Practice1/views.py
+++ Practice1/views.py
@@ -4,4 +4,8 @@ views = Blueprint(__name__, "views")
 
 @views.route("/")
 def home():
-    return render_template("index.html", name="Shakti", age=21)
+    return render_template("index.html", name="Shakti")
+
+@views.route("/profile/<username>")
+def profile(username):
+    return render_template("index.html", name=username)
